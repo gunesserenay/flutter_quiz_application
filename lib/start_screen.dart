6e8daa771a6bc42bea9ba655a3de6f23fedd546b
Serenay <serenayygunes@gmail.com>
2023-10-03 20:39:34 +0300

    Two way opacity added to image

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -11,7 +11,16 @@ class StartScreen extends StatelessWidget {
           Image.asset(
             'assets/images/quiz-logo.png',
             width: 300,
+            color: const Color.fromARGB(80, 255, 255, 255),
           ),
+
+          // Opacity(
+          //   opacity: 0.6,
+          //   child: Image.asset(
+          //     'assets/images/quiz-logo.png',
+          //     width: 300,
+          //   ),
+          // ),
           const SizedBox(height: 80),
           const Text(
             "Learn Flutter",
